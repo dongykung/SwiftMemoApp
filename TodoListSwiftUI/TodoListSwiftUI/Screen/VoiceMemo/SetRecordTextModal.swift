cb98f7ae6cd6a5a31ad3abdb173bef895210e809
kim dong kyung <kdkd422@naver.com>
2024-07-24 22:58:54 +0900

    [Add] 녹음 종료 시 파일 제목 입력 받는 모달 추가

--- TodoListSwiftUI/TodoListSwiftUI/Screen/VoiceMemo/SetRecordTextModal.swift
+++ TodoListSwiftUI/TodoListSwiftUI/Screen/VoiceMemo/SetRecordTextModal.swift
@@ -8,11 +8,39 @@
 import SwiftUI
 
 struct SetRecordTextModal: View {
+    @Binding var recordModalVisible: Bool
+    @ObservedObject var voiceViewModel: VoiceRecorderViewModel
+    @State var recordTitle: String = ""
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            HStack {
+                Text("녹음 파일 제목을 입력하세요")
+                    .font(.title3)
+                    .padding(.leading,12)
+                Spacer()
+            }
+            TextField("녹음 파일 제목을 입력하세요.",text: $recordTitle)
+                .cornerRadius(8.0)
+                .padding(.horizontal,12)
+                .padding(.vertical,6)
+        }
+        HStack {
+            Spacer()
+            Button("저장") {
+                voiceViewModel.endRecord(recordTitle)
+                recordModalVisible = false
+            }
+            Spacer()
+            Button("취소") {
+              recordModalVisible = false
+            }
+            Spacer()
+        }
+        .font(.title3)
     }
+    
 }
 
 #Preview {
-    SetRecordTextModal()
+    SetRecordTextModal(recordModalVisible: .constant(false),voiceViewModel: VoiceRecorderViewModel())
 }
